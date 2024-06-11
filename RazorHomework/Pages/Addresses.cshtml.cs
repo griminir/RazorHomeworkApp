5678dc6a76b29b37bd7f8b2930aa0aa390a1f73c
griminir <viktor.elliot.king@gmail.com>
2024-06-11 22:55:13 +0200

    Add project files.

---
+++ RazorHomework/Pages/Addresses.cshtml.cs
@@ -0,0 +1,25 @@
+using DemoLibrary;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorHomework.Pages
+{
+    public class AddressesModel : PageModel
+    {
+        [BindProperty]
+        public AddressModel Address { get; set; }
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid == false)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("./index");
+        }
+    }
+}
